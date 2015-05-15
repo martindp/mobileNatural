ceb475a9b45508d2781e16bb74c1cdf8f3b4cb4d
dromi29 <dromi29@hotmail.com>
2015-05-15 18:50:16 -0300

    pedido producto productor productosproductor

--- www/js/productor/productor-controller.js
+++ www/js/productor/productor-controller.js
@@ -3,6 +3,54 @@ appModule
         $scope.productores = Productores.obtenerProductores();
     })
 
-    .controller('ProductorCtrl', function($scope, $stateParams, Productores) {
-        $scope.productor = Productores.obtenerProductorPorId($stateParams.productorId)
+    .controller('ProductorCtrl', function($scope, $http, $stateParams, Productores, Consultas,$resource, $localstorage) {
+        $scope.productor = Productores.obtenerProductorPorId($stateParams.productorId);
+
+        $scope.descripcion = true;
+        $scope.comentarios = false;
+        $scope.consultas = false;
+        $scope.mostrarDescripcion = function () {
+            $scope.descripcion = true;
+            $scope.comentarios = false;
+            $scope.consultas = false;
+        };
+        $scope.mostrarComentarios = function () {
+            $scope.descripcion = false;
+            $scope.comentarios = true;
+            $scope.consultas = false;
+        };
+        $scope.mostrarConsultas = function () {
+            $scope.descripcion = false;
+            $scope.comentarios = false;
+            $scope.consultas = true;
+        };
+
+        $scope.nuevaConsulta = false;
+        $scope.consulta = { Descripcion: '' };
+        $scope.crearConsulta = function () {
+            $scope.nuevaConsulta = true;
+        };
+        $scope.guardarConsulta = function (productorId, descripcion) {
+            //Consultas.guardarConsulta(descripcion).$promise.then(
+            //    function () {
+            //        $scope.nuevaConsulta = false;
+            //        //actualizar consultas
+            //        $scope.consulta = { Descripcion: '' };
+            //    },
+            //    function (response) { $scope.errors = response.data; });
+            $http({ method: 'POST', isArray: false, url: 'http://localhost:51297/api/ConsultasProductor/PostConsultas', params: { productorId: productorId, descripcion: descripcion } }).then(
+                function () {
+                    $scope.nuevaConsulta = false;
+                    $scope.consulta = { Descripcion: '' };
+                    //Actualizo
+                    $scope.productor = Productores.obtenerProductorPorId($stateParams.productorId);
+
+                });
+        };
+        $scope.cancelarConsulta = function (){
+            $scope.nuevaConsulta = false;
+            $scope.consulta = { Descripcion: '' };
+        };
     })
+
+
--- www/js/productor/productor-factory.js
+++ www/js/productor/productor-factory.js
@@ -6,6 +6,11 @@ appModule
           },
           obtenerProductorPorId: function(id){
               return $resource('http://localhost:51297/api/Productores/'+id).get();
+          },
+
+          guardarConsulta: function (descripcion) {
+              return $resource('http://localhost:51297/api/ConsultasProductor/PostConsultas').save(descripcion);
           }
+
       }
     })
