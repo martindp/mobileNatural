67b3c8ca7bcac004d56eb23002ed79e7bb20ad2a
Proyectos <matiasdieg91@gmail.com>
2015-07-15 22:01:09 -0300

    Login de usuario con listado pedido

--- www/js/pedido/pedido-factory.js
+++ www/js/pedido/pedido-factory.js
@@ -1,11 +1,14 @@
 appModule
-    .factory('Pedidos', function($resource) {
+    .factory('Pedidos', function($resource,$localstorage) {
         return {
             realizarPedido: function (pedidos) {
                 return $resource(apiUrl+'/api/Pedidos/RealizarPedido').save(pedidos);
             },
             obtenerPedidos: function () {
-                return $resource(apiUrl+'/api/Pedidos').query();
+                var authData = $localstorage.getObject('authorizationData');
+                var data = "Bearer "+authData.token;
+                var user = authData.userName;
+                return $resource(apiUrl+'/api/Pedidos',{usuario:user},{ 'query': {method:'GET' ,isArray:true,headers: { 'Authorization': data } }}).query();
             },
             obtenerPedidoPorPedidoId: function (id) {
                 return $resource(apiUrl+'/api/Comentarios/'+id).get();
